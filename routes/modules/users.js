e2a383037a6e74a62158596c78f846ddc32c9c57
Marcus Liu <me@mail.lmarcus.onl>
2022-04-10 02:52:43 +0800

    feat: 登出路由

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -59,5 +59,9 @@ router.post('/login', passport.authenticate('local', {
   failureRedirect: '/users/login'
 }))
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
 
 module.exports = router
