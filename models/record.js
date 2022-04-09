a4686eff5d93e612ac1e9e4e7e27dcbe329ea341
Marcus Liu <me@mail.lmarcus.onl>
2022-04-09 15:29:47 +0800

    feat(data_model): record schema

---
+++ models/record.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema({
+  id: {
+    type: Number,
+    required: true
+  }
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,s
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: Number,
+    required: true
+  },
+  categoryId: {
+    type: Number,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
