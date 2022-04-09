8ffbf9ecddbd15f8d690cc6ed27cc44b51bf5db7
Marcus Liu <me@mail.lmarcus.onl>
2022-04-10 02:27:23 +0800

    feat: 註冊功能與 views/partial

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const routes = require('./routes')
+require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -7,6 +9,48 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '全部欄位必須填入！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  User.findOne({ email }).then(user => {
+    if (user) {
+      errors.push({ message: 'Email 已被註冊！' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 module.exports = router
