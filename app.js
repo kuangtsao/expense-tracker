7cf6458eceec3ad7a0175657ef84b219ec667da6
Marcus Liu <me@mail.lmarcus.onl>
2022-04-10 06:12:36 +0800

    feat: 登入/登出功能

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
+// 載入環境變數
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 // express setting
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // express-handlebars setting
+const session = require('express-session')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const flash = require('connect-flash')
+const routes = require('./routes')
+require('./config/mongoose')
+const usePassport = require('./config/passport')
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
-// routes
-app.get('/', (req, res) => {
-  res.render('index')
+usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.loginError = req.flash('loginError')
+  next()
 })
+// routes
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`express is running on http://localhost:${port}`)
---
+++ routes/modules/users.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/user')
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '全部欄位必須填入！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+  User.findOne({ email }).then(user => {
+    if (user) {
+      errors.push({ message: 'Email 已被註冊！' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+})
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login',
+  failureFlash: true
+}))
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', '已成功登出')
+  res.redirect('/users/login')
+})
+
+module.exports = router
