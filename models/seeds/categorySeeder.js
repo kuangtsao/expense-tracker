7bfbbc81ec8a3b5daec9f1326a9e701c7c8caaa4
Marcus Liu <me@mail.lmarcus.onl>
2022-04-09 17:16:28 +0800

    feat(data_model): 完成 categorySeeder.js

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,4 +1,3 @@
-
 const db = require('../../config/mongoose')
 const User = require('../user')
 const Category = require('../category')
@@ -6,12 +5,14 @@ const Category = require('../category')
 const SEED_USER = [
   {
     id: 1,
+    name: '廣志',
     email: 'hirosi@kasukabe.saitama',
     password: '12345678'
   },
   {
     id: 2,
-    email: 'shinnosuke@kasukane.saitama',
+    name: '小新',
+    email: 'shinnosuke@kasukabe.saitama',
     password: '12345678'
   }
 ]
@@ -36,10 +37,46 @@ const SEED_CATEGORY = [
     id: 4,
     name: '餐飲',
     icon: 'fa-utensils'
-  }
+  },
   {
     id: 5,
     name: '其他',
     icon: 'fa-pen'
   }
 ]
+
+// TODO:
+// 1. 確保只會建立一筆資料
+// 2. 密碼利用 bcrypt hash
+
+db.once('open', () => {
+  const createSeedUser = new Promise((resolve, reject) => {
+    SEED_USER.forEach(userInfo => {
+      User.create({
+        id: userInfo.id,
+        name: userInfo.name,
+        email: userInfo.email,
+        password: userInfo.password
+      })
+    })
+    resolve()
+  })
+
+  const createSeedCategory = new Promise((resolve, reject) => {
+    SEED_CATEGORY.forEach(categoryInfo => {
+      Category.create({
+        id: categoryInfo.id,
+        name: categoryInfo.name,
+        icon: categoryInfo.icon
+      })
+    })
+    resolve()
+  })
+
+  Promise.all([createSeedUser, createSeedCategory])
+    .then(() => {
+      console.log('user & category created!')
+      process.exit()
+    })
+    .catch(error => console.log(error))
+})
