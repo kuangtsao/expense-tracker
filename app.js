66cebe0ccbd4cc674f9770b90ceae317e6046485
Marcus Liu <me@mail.lmarcus.onl>
2022-04-10 05:18:12 +0800

    refactor: 修改 port 利用環境變數獲得

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 // express setting
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // express-handlebars setting
 const session = require('express-session')
