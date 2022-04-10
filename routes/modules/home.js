333762a8d11c91dc5f975237f4720119a820b3e7
Marcus Liu <me@mail.lmarcus.onl>
2022-04-10 12:22:38 +0800

    feat: post /records/new

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -17,7 +17,6 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   const userId = req.user._id
   const categoryOption = req.body.categoryOption
-  console.log(categoryOption)
   const selectedCategory = await Category.findById(categoryOption).lean()
   const otherCategory = await Category.find({ _id: {$ne: categoryOption} }).lean()
   const records = await Record.find({ userId, categoryId: categoryOption })
@@ -25,8 +24,6 @@ router.post('/', async (req, res) => {
                               .lean()
   let totalAmount = 0
   records.forEach(item => totalAmount += item.amount)
-  console.log(selectedCategory)
-  console.log(otherCategory)
   res.render('index', { selectedCategory, otherCategory, records, totalAmount })
 })
 
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,11 +3,23 @@ const category = require('../../models/category')
 const router = express.Router()
 
 const Category = require('../../models/category')
+const Record = require('../../models/record')
 
 router.get('/new', async (req, res) => {
   const category = await Category.find().lean()
-
   res.render('new', { category })
+})
+
+router.post('/new', (req, res) => {
+  const userId = req.user._id
+  console.log(req.body)
+  const { name, categoryId, date, amount } = req.body
+  console.log(`${name},${date},${amount},${userId},${categoryId}`)
+
+  return Record.create({ name, date, amount, userId, categoryId})
+               .then(() => res.redirect('/'))
+               .catch(error => console.error(error))
+ 
 })
 
 module.exports = router
