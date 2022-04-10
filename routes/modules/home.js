ae33a1ae39f071295628089a05c16dfc6ac1203d
Marcus Liu <me@mail.lmarcus.onl>
2022-04-10 10:48:27 +0800

    feat: post / 依據不同類別顯示清單以及總和

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,7 +10,24 @@ router.get('/', async (req, res) => {
   const category = await Category.find().lean()
   let totalAmount = 0 
   records.forEach(item => totalAmount += item.amount)
-  res.render('index', { category, records, totalAmount})
+  res.render('index', { category, records, totalAmount })
+})
+
+// 選擇類別以顯示部分清單
+router.post('/', async (req, res) => {
+  const userId = req.user._id
+  const categoryOption = req.body.categoryOption
+  console.log(categoryOption)
+  const selectedCategory = await Category.findById(categoryOption).lean()
+  const otherCategory = await Category.find({ _id: {$ne: categoryOption} }).lean()
+  const records = await Record.find({ userId, categoryId: categoryOption })
+                              .populate('categoryId')
+                              .lean()
+  let totalAmount = 0
+  records.forEach(item => totalAmount += item.amount)
+  console.log(selectedCategory)
+  console.log(otherCategory)
+  res.render('index', { selectedCategory, otherCategory, records, totalAmount })
 })
 
 module.exports = router
